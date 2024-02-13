1108b054174247de47a5781cb309c3ac29660b8f
Luiz César Pacheco <lcpsantos@gmail.com>
2024-02-13 10:01:03 -0300

    Removendo redundancias.

--- src/main/java/br/com/alura/adopet/api/model/Tutor.java
+++ src/main/java/br/com/alura/adopet/api/model/Tutor.java
@@ -1,6 +1,5 @@
 package br.com.alura.adopet.api.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -15,25 +14,20 @@ public class Tutor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
     @NotBlank
-    @Column(name = "nome")
     private String nome;
 
     @NotBlank
     @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}")
-    @Column(name = "telefone")
     private String telefone;
 
     @NotBlank
     @Email
-    @Column(name = "email")
     private String email;
 
     @OneToMany(mappedBy = "tutor", fetch = FetchType.EAGER)
-    @JsonManagedReference("tutor_adocoes")
     private List<Adocao> adocoes;
 
     @Override
